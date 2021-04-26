37a8f3018e835b2889e599753fae11a7f7d7f3a5
oleongomez <66493900+oleongomez@users.noreply.github.com>
2021-04-25 21:41:01 -0700

    Animation needs work on boundaries and takes to long (#2)

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
-const create_square = (vertical_boundary, horizontal_boundary) => {
+const create_square = (vertical_boundary, horizontal_boundary, id) => {
     let position_x = Math.floor(Math.random() * vertical_boundary);
     let position_y = Math.floor(Math.random() * horizontal_boundary);
     let velocity_x = Math.floor(Math.random() * 5);
     let velocity_y = Math.floor(Math.random() * 5);
     let direction = 1
     var div = document.createElement('div');
+    div.id = id.toString()
     div.classList.add("square")
     div.style.left = position_x + 'px';    
     div.style.top = position_y + 'px';    
     div.style.borderRadius = '50%'; 
-    console.log("Position: ",position_x,position_y)
     return {
       position_x: position_x,
       position_y: position_y,
@@ -30,30 +30,35 @@ const create_square = (vertical_boundary, horizontal_boundary) => {
   };
   
   const move = (square) => {
+      console.log('move')
     square.position_x += square.velocity_x;
     square.position_y += square.velocity_y;
+    square.div.style.left = square.position_x+"px";
+    square.div.style.top = square.position_y+"px";
+
   };
   
   const change_direction = (vertical_boundary, square) => {
     if(detectCollisionWithVertical(horizontal_boundary, square))
-      square.direction = 0
+      square.direction = 1
   };
   var squares = []
   const create_squares = (number_of_squares) => {
       var animation = document.getElementById('animation')
       for(let i=0;i<number_of_squares; ++i){
-          console.log(animation.clientWidth, animation.clientHeight)
-          var square = create_square(animation.clientWidth, animation.clientHeight)
-          squares.push()
+          var square = create_square(animation.clientWidth, animation.clientHeight,i)
+          squares.push(square)
           animation.appendChild(square.div)
       }
 
 
   }
   create_squares(500)
-  const animate = () => {
+const animate = () => {
+    squares.forEach(move)
 
   }
+  setInterval(animate,1000/24)
 exports = {
     create_square,
     detectCollisionWithVertical,
